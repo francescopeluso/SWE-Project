b4b40f74d2fbfbe6aea77928a348c038ecdb871f
Gerardo Selce <gerardoselce28@gmail.com>
2024-11-29 09:54:20 +0100

    Modifica classe PhoneNumber

--- src/main/java/g20swe/addressbook/model/contacts/PhoneNumber.java
+++ src/main/java/g20swe/addressbook/model/contacts/PhoneNumber.java
@@ -1,5 +1,34 @@
 package g20swe.addressbook.model.contacts;
 
-public class PhoneNumber {
+import g20swe.addressbook.validation.Validatable;
+import java.util.regex.Pattern;
+
+public class PhoneNumber implements Validatable{
+    
+    private String phoneNumber;
+    
+    public PhoneNumber(){
+        this.phoneNumber = null;
+    }
+    
+    public PhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+    
+    public void setPhoneNumber(){
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean validate() {
+        String numberRegex = "^(\\+?\\d{1,4}[\\s\\-]?)?(\\(?\\d{2,4}\\)?[\\s\\-]?)?[\\d\\s\\-]{6,10}$";
+        
+        return Pattern.matches(numberRegex, this.phoneNumber);
+    }
+    
     
 }
