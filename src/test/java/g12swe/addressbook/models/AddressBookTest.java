6f24e592a36b9865b73c19163f06100bf1691dec
Francesco Peluso <59664699+francescopeluso@users.noreply.github.com>
2024-12-11 16:54:46 +0100

    Risolto conflitto casi di test JUnit

--- src/test/java/g12swe/addressbook/models/AddressBookTest.java
+++ src/test/java/g12swe/addressbook/models/AddressBookTest.java
@@ -6,11 +6,8 @@ package g12swe.addressbook.models;
 
 import g12swe.addressbook.models.contacts.Contact;
 import java.util.Set;
-<<<<<<< HEAD
-=======
 import java.util.TreeSet;
 import javafx.collections.FXCollections;
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,11 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
-<<<<<<< HEAD
- * @author giaro
-=======
  * @author Valerio
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
  */
 public class AddressBookTest {
     
@@ -33,11 +26,6 @@ public class AddressBookTest {
     
     @BeforeAll
     public static void setUpClass() {
-<<<<<<< HEAD
-        
-        
-=======
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
     
     @AfterAll
@@ -58,22 +46,10 @@ public class AddressBookTest {
     @Test
     public void testAddContact() {
         System.out.println("addContact");
-<<<<<<< HEAD
-        Contact c = new Contact("Lady", "Gaga");
-        AddressBook instance = new AddressBook();
-        
-        instance.addContact(c);
-        
-        assertTrue(instance.getContactList().contains(new Contact("Lady", "Gaga")));
-        
-        // TODO review the generated test code and remove the default call to fail.
-        
-=======
         Contact c = new Contact("Stefani", "Germanotta");
         AddressBook instance = new AddressBook();
         instance.addContact(c);
         assertTrue(instance.getContactList().contains(c));
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
@@ -82,23 +58,12 @@ public class AddressBookTest {
     @Test
     public void testRemoveContact() {
         System.out.println("removeContact");
-<<<<<<< HEAD
-        Contact c = new Contact("Jay", "Z");
-        AddressBook instance = new AddressBook();
-        
-=======
         Contact c = new Contact("Stefani", "Germanotta");
         AddressBook instance = new AddressBook();
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
         instance.addContact(c);
         boolean expResult = true;
         boolean result = instance.removeContact(c);
         assertEquals(expResult, result);
-<<<<<<< HEAD
-        // TODO review the generated test code and remove the default call to fail.
-       
-=======
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
@@ -108,20 +73,6 @@ public class AddressBookTest {
     public void testGetContactList() {
         System.out.println("getContactList");
         AddressBook instance = new AddressBook();
-<<<<<<< HEAD
-        Set<Contact> expResult = null;
-        Set<Contact> result = instance.getContactList();
-        
-        instance.addContact(new Contact("Gianni", "Sperti"));
-        
-        assertTrue(instance.getContactList().contains(new Contact("Gianni", "Sperti")));
-        expResult = instance.getContactList();
-        assertEquals(expResult, result);
-        
-        
-        
-        // TODO review the generated test code and remove the default call to fail.
-=======
         Contact c = new Contact("Stefani", "Germanotta");
         Contact c2 = new Contact("Beyoncé", "Knowles");
         instance.addContact(c);
@@ -132,7 +83,6 @@ public class AddressBookTest {
         Set<Contact> result = instance.getContactList();
         
         assertEquals(FXCollections.observableSet(expResult), result);
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
     
 }
--- src/test/java/g12swe/addressbook/models/contacts/ContactTest.java
+++ src/test/java/g12swe/addressbook/models/contacts/ContactTest.java
@@ -4,12 +4,9 @@
  */
 package g12swe.addressbook.models.contacts;
 
-<<<<<<< HEAD
-=======
 import g12swe.addressbook.exceptions.InvalidEmailAddressException;
 import g12swe.addressbook.exceptions.InvalidPhoneNumberException;
 import java.util.ArrayList;
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
@@ -20,11 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
-<<<<<<< HEAD
- * @author giaro
-=======
  * @author Valerio
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
  */
 public class ContactTest {
     
@@ -53,19 +46,10 @@ public class ContactTest {
     @Test
     public void testGetName() {
         System.out.println("getName");
-<<<<<<< HEAD
-        Contact instance = null;
-        String expResult = "";
-        String result = instance.getName();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
         Contact instance = new Contact("Stefani", "Germanotta");
         String expResult = "Stefani";
         String result = instance.getName();
         assertEquals(expResult, result);
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
@@ -74,35 +58,16 @@ public class ContactTest {
     @Test
     public void testGetSurname() {
         System.out.println("getSurname");
-<<<<<<< HEAD
-        Contact instance = null;
-        String expResult = "";
-        String result = instance.getSurname();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
         Contact instance = new Contact("Stefani", "Germanotta");
         String expResult = "Germanotta";
         String result = instance.getSurname();
         assertEquals(expResult, result);
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
      * Test of getPhoneNumbers method, of class Contact.
      */
     @Test
-<<<<<<< HEAD
-    public void testGetPhoneNumbers() {
-        System.out.println("getPhoneNumbers");
-        Contact instance = null;
-        List<PhoneNumber> expResult = null;
-        List<PhoneNumber> result = instance.getPhoneNumbers();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
     public void testGetPhoneNumbers() throws InvalidPhoneNumberException {
         System.out.println("getPhoneNumbers");
         Contact instance = new Contact("Stefani", "Germanotta");
@@ -122,23 +87,12 @@ public class ContactTest {
         expResult.add(new PhoneNumber("1111153456", EntryCategory.WORK));
         result = instance.getPhoneNumbers();
         assertNotEquals(expResult, result);
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
      * Test of getEmailAddresses method, of class Contact.
      */
     @Test
-<<<<<<< HEAD
-    public void testGetEmailAddresses() {
-        System.out.println("getEmailAddresses");
-        Contact instance = null;
-        List<EmailAddress> expResult = null;
-        List<EmailAddress> result = instance.getEmailAddresses();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
     public void testGetEmailAddresses() throws InvalidEmailAddressException {
         System.out.println("getEmailAddresses");
         Contact instance = new Contact("Stefani", "Germanotta");
@@ -153,7 +107,6 @@ public class ContactTest {
         result = instance.getEmailAddresses();
         expResult.add(new EmailAddress("uwu2@gmail.com", EntryCategory.WORK));
         assertNotEquals(expResult, result);
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
@@ -162,20 +115,12 @@ public class ContactTest {
     @Test
     public void testSetName() {
         System.out.println("setName");
-<<<<<<< HEAD
-        String name = "";
-        Contact instance = null;
-        instance.setName(name);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
         String name = "Stefani";
         Contact instance = new Contact("STEFANIA", "Germanotta");
         instance.setName(name);
         assertEquals(name, instance.getName());
         
         assertNotEquals("STEFANIA", instance.getName());
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
@@ -184,20 +129,12 @@ public class ContactTest {
     @Test
     public void testSetSurname() {
         System.out.println("setSurname");
-<<<<<<< HEAD
-        String surname = "";
-        Contact instance = null;
-        instance.setSurname(surname);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
         String surname = "Germanotta";
         Contact instance = new Contact("Stefani", "GERMANIA");
         instance.setSurname(surname);
         assertEquals(surname, instance.getSurname());
         
         assertNotEquals("GERMANIA", instance.getSurname());
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
@@ -206,20 +143,11 @@ public class ContactTest {
     @Test
     public void testAddEmailAddress() throws Exception {
         System.out.println("addEmailAddress");
-<<<<<<< HEAD
-        String emailAddress = "";
-        EntryCategory category = null;
-        Contact instance = null;
-        instance.addEmailAddress(emailAddress, category);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
         String emailAddress = "mariorossi@gmail.com";
         Contact instance = new Contact("Mario", "Rossi");
         instance.addEmailAddress(emailAddress, EntryCategory.WORK);
         
         assertTrue(instance.getEmailAddresses().contains(new EmailAddress(emailAddress, EntryCategory.WORK)));
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
@@ -228,35 +156,17 @@ public class ContactTest {
     @Test
     public void testAddPhoneNumber() throws Exception {
         System.out.println("addPhoneNumber");
-<<<<<<< HEAD
-        String phoneNumber = "";
-        EntryCategory category = null;
-        Contact instance = null;
-        instance.addPhoneNumber(phoneNumber, category);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
         String phoneNumber = "1234567890";
         Contact instance = new Contact("Mario", "Rossi");
         instance.addPhoneNumber(phoneNumber, EntryCategory.WORK);
         
         assertTrue(instance.getPhoneNumbers().contains(new PhoneNumber(phoneNumber, EntryCategory.WORK)));
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
      * Test of removeEmailAddress method, of class Contact.
      */
     @Test
-<<<<<<< HEAD
-    public void testRemoveEmailAddress() {
-        System.out.println("removeEmailAddress");
-        String emailAddress = "";
-        Contact instance = null;
-        instance.removeEmailAddress(emailAddress);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
     public void testRemoveEmailAddress() throws InvalidEmailAddressException {
         System.out.println("removeEmailAddress");
         String emailAddress = "mariorossi@gmail.com";
@@ -266,22 +176,12 @@ public class ContactTest {
         
         instance.removeEmailAddress(emailAddress);
         assertFalse(instance.getEmailAddresses().contains(new EmailAddress(emailAddress, EntryCategory.WORK)));
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
      * Test of removePhoneNumber method, of class Contact.
      */
     @Test
-<<<<<<< HEAD
-    public void testRemovePhoneNumber() {
-        System.out.println("removePhoneNumber");
-        String phoneNumber = "";
-        Contact instance = null;
-        instance.removePhoneNumber(phoneNumber);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
     public void testRemovePhoneNumber() throws InvalidPhoneNumberException {
         System.out.println("removePhoneNumber");
         String phoneNumber = "1234567890";
@@ -292,7 +192,6 @@ public class ContactTest {
         
         instance.removePhoneNumber(phoneNumber);
         assertFalse(instance.getPhoneNumbers().contains(new PhoneNumber(phoneNumber, EntryCategory.WORK)));
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
@@ -301,19 +200,10 @@ public class ContactTest {
     @Test
     public void testHashCode() {
         System.out.println("hashCode");
-<<<<<<< HEAD
-        Contact instance = null;
-        int expResult = 0;
-        int result = instance.hashCode();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
         Contact instance = new Contact("Mario", "Rossi");
         int expResult = new Contact("Mario", "Rossi").hashCode();
         int result = instance.hashCode();
         assertNotEquals(expResult, result);
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
@@ -323,14 +213,6 @@ public class ContactTest {
     public void testEquals() {
         System.out.println("equals");
         Object obj = null;
-<<<<<<< HEAD
-        Contact instance = null;
-        boolean expResult = false;
-        boolean result = instance.equals(obj);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
         Contact instance = new Contact("Mario", "Rossi");
         boolean expResult = false;
         boolean result = instance.equals(obj);   //confronto un contatto con un 
@@ -354,7 +236,6 @@ public class ContactTest {
         result = instance.equals(obj);    //confronto il contatto con se stesso
         assertEquals(expResult, result);
                                            
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
 
     /**
@@ -362,16 +243,6 @@ public class ContactTest {
      */
     @Test
     public void testCompareTo() {
-<<<<<<< HEAD
-        System.out.println("compareTo");
-        Contact o = null;
-        Contact instance = null;
-        int expResult = 0;
-        int result = instance.compareTo(o);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-=======
         
         System.out.println("compareTo");           //compara solo il cognome
         Contact o = new Contact("Stefani", "B"); 
@@ -391,7 +262,6 @@ public class ContactTest {
         o.setSurname("C");                // A > B
         result = instance.compareTo(o); 
         assertEquals(expResult, result);
->>>>>>> 95a0983e8182ef3957ddd12e2a89b40d0495e734
     }
     
 }
