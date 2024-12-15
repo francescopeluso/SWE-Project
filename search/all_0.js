3a33f737e7cf47f79a018bbb8547a97388293cdf
francescopeluso <francescopeluso@users.noreply.github.com>
2024-12-15 18:06:55 +0000

    deploy: b9fbd48c1960524bb6487df6b6ee9bea93564516

--- search/all_0.js
+++ search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['addcontact_0',['addContact',['../classg12swe_1_1addressbook_1_1controllers_1_1MainController.html#aba4e368c4e5e69c468e8e3c1f8446acf',1,'g12swe.addressbook.controllers.MainController.addContact()'],['../classg12swe_1_1addressbook_1_1models_1_1AddressBook.html#ae76d4a3f30b48ebc06d25d058145bce7',1,'g12swe.addressbook.models.AddressBook.addContact()']]],
+  ['addcontact_0',['addContact',['../classg12swe_1_1addressbook_1_1controllers_1_1MainController.html#a32f11138bfbf6bb7dc111bd517f4881e',1,'g12swe.addressbook.controllers.MainController.addContact()'],['../classg12swe_1_1addressbook_1_1models_1_1AddressBook.html#ae76d4a3f30b48ebc06d25d058145bce7',1,'g12swe.addressbook.models.AddressBook.addContact()']]],
   ['addemailaddress_1',['addEmailAddress',['../classg12swe_1_1addressbook_1_1models_1_1contacts_1_1Contact.html#a3d11e8c09da7fd9824cde7233cc57b57',1,'g12swe::addressbook::models::contacts::Contact']]],
   ['addphonenumber_2',['addPhoneNumber',['../classg12swe_1_1addressbook_1_1models_1_1contacts_1_1Contact.html#aa946fbcc766f81d628457ef90019832b',1,'g12swe::addressbook::models::contacts::Contact']]],
   ['addressbook_3',['AddressBook',['../classg12swe_1_1addressbook_1_1models_1_1AddressBook.html#aeb8656cd1b0c1cc3502561f8e69d5971',1,'g12swe.addressbook.models.AddressBook.AddressBook()'],['../classg12swe_1_1addressbook_1_1models_1_1AddressBook.html',1,'g12swe.addressbook.models.AddressBook']]],
