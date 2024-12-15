d22b5d6ceabb90f82c6631cc5b416e71f27f41aa
Francesco Peluso <59664699+francescopeluso@users.noreply.github.com>
2024-12-15 23:41:00 +0100

    Aggiornamento documentazione

--- docs/doxygen/html/search/all_8.js
+++ docs/doxygen/html/search/all_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['hashcode_0',['hashCode',['../classg12swe_1_1addressbook_1_1models_1_1contacts_1_1_contact.html#a7b2b014121a29839afc434d6dbfd06a8',1,'g12swe.addressbook.models.contacts.Contact.hashCode()'],['../classg12swe_1_1addressbook_1_1models_1_1contacts_1_1_email_address.html#a8cdf1775b4a6ebbe768889ec7c77d798',1,'g12swe.addressbook.models.contacts.EmailAddress.hashCode()'],['../classg12swe_1_1addressbook_1_1models_1_1contacts_1_1_phone_number.html#aa1fb6169ef41fb9d84ce3788951861e8',1,'g12swe.addressbook.models.contacts.PhoneNumber.hashCode()']]]
+  ['handlecontactupdated_0',['handleContactUpdated',['../classg12swe_1_1addressbook_1_1controllers_1_1_main_controller.html#a59ed363aaa8915530026949750980755',1,'g12swe::addressbook::controllers::MainController']]],
+  ['hashcode_1',['hashCode',['../classg12swe_1_1addressbook_1_1models_1_1contacts_1_1_contact.html#a7b2b014121a29839afc434d6dbfd06a8',1,'g12swe.addressbook.models.contacts.Contact.hashCode()'],['../classg12swe_1_1addressbook_1_1models_1_1contacts_1_1_email_address.html#a8cdf1775b4a6ebbe768889ec7c77d798',1,'g12swe.addressbook.models.contacts.EmailAddress.hashCode()'],['../classg12swe_1_1addressbook_1_1models_1_1contacts_1_1_phone_number.html#aa1fb6169ef41fb9d84ce3788951861e8',1,'g12swe.addressbook.models.contacts.PhoneNumber.hashCode()']]]
 ];
